e8de78c0422c40fc53819e96fa0c46e9b974cd84
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-05-17 11:03:22 +0500

    serverga tayorlandi

--- journal_ubtuit/settings.py
+++ journal_ubtuit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!e_z@noeor1xux(iw#+t_(+4db=4q44%x)%5#j=85waa!gl%%_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
